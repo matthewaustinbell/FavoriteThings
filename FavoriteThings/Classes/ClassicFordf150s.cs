8ce80a14e95bc2cf5121fac459fdfd714324322e
unknown <matthewaustinbell@gmail.com>
2019-09-03 15:07:35 -0500

    favorite classes added

---
+++ FavoriteThings/Classes/ClassicFordf150s.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FavoriteThings.NewFolder
+{
+    class ClassicFordf150s
+    {
+        public string Color { get; set; }
+
+        public int TopSpeed { get; set; }
+
+        public bool isRunning { get; set; }
+
+    }
+}
---
+++ FavoriteThings/Classes/RadnorLake.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FavoriteThings.NewFolder
+{
+    class RadnorLake
+    {
+        public string Name { get; set; }
+        public int Calories { get; set; }
+    }
+}
